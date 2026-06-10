034c42ac93611b62224e0b6905cb92668ae84085
Anesszereg <anesszereg1@gmail.com>
2026-06-10 11:14:26 +0100

    change laungauge from en to fr

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Foot Zone application', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const appElement = screen.getByText(/Foot Zone/i);
+  expect(appElement).toBeInTheDocument();
 });
